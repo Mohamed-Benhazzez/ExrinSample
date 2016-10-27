af55fe71e02faf713c47384953c7b2e11e5d7806
Adam Pedley <adam.pedley@gmail.com>
2016-10-27 22:56:53 +1100

    Fixed typo, single top launch mode

--- Exrin-Sample/ExrinSample.Droid/MainActivity.cs
+++ Exrin-Sample/ExrinSample.Droid/MainActivity.cs
@@ -1,10 +1,10 @@
 ﻿using Android.App;
-using Android.Widget;
+using Android.Content.PM;
 using Android.OS;
 
 namespace ExrinSample.Droid
 {
-    [Activity(Label = "ExrinSample.Droid", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/MainTheme")]
+    [Activity(Label = "ExrinSample.Droid", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/MainTheme", LaunchMode = LaunchMode.SingleTop)]
     public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)

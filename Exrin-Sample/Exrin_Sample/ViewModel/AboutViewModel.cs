833e32db2772cda06b1b3fcbfa1b98aa46a4c564
Adam Pedley <adam.pedley@gmail.com>
2016-12-24 16:00:11 +1100

    Exrin 1.0.4-beta3 - tested SilentPop

--- Exrin-Sample/Exrin_Sample/ViewModel/AboutViewModel.cs
+++ Exrin-Sample/Exrin_Sample/ViewModel/AboutViewModel.cs
@@ -3,12 +3,22 @@
     using Abstraction.Model;
     using Exrin.Abstraction;
     using Base;
+    using System.Collections.Generic;
 
     public class AboutViewModel : BaseViewModel
     {
+       
         public AboutViewModel(IMainModel model, IExrinContainer exrinContainer) 
             : base(exrinContainer, new AboutVisualState(model))
-        {            
+        {
         }
+
+        //public override async void OnAppearing()
+        //{
+        //    // Testing a SilentPop when coming to this page
+        //    base.OnAppearing();
+        //    await _navigationService.SilentPop(Abstraction.Enums.Stack.Main, new List<string>() { Abstraction.Enums.Views.Main.Main.ToString() });
+        //}
+
     }
 }
